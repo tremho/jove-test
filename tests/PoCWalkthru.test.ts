17b325b5dd01c8e23c1b1cee463198a2c4499fe7
Tremho <steve@ohmert.com>
2021-11-07 13:01:51 -0800

    restored tests

--- tests/PoCWalkthru.test.ts
+++ tests/PoCWalkthru.test.ts
@@ -13,29 +13,29 @@ async function pocTest(t:any) {
     await testRemote(t, 'divide 714 17', 'division test', 42)
     await testRemote(t, 'greet Steve', 'text return test', 'hello, Steve')
     await testRemote(t, 'fetch', 'async return test', 'Okay, here I am')
-//     let navInfo = await callRemote('readModelValue page.navInfo')
-//     t.ok(typeof navInfo === 'object', 'navInfo is an object (got '+typeof navInfo+') '+navInfo)
-//     t.ok(navInfo.pageId === 'main', 'Page is main (got '+navInfo.pageId+')')
-//     t.ok(navInfo.timestamp > 0 && navInfo.timestamp < Date.now(), 'Timestamp is valid (got '+navInfo.timestamp+')')
-//     let firstTimestamp = navInfo.timestamp
-//
-//     await testRemote(t, 'assignComponent hiLabel simple-label', 'assign hiLabel', true)
-//     await testRemote(t, 'assignComponent changer simple-button action changeValues', 'assign changer', true)
-//     await testRemote(t, 'assignComponent nextbtn simple-button action nextPage', 'assign nextbtn', true)
-//     await testRemote(t, 'readComponentProperty changer text', 'verify component', 'Change Values')
-//     await testRemote(t, 'triggerAction changer', 'press button', true)
-//
-//     await testRemote(t, 'readModelValue greeting.hello', 'verify change at model', 'Howdy')
-// //    await testRemote(t, 'readComponentProperty hiLabel text', 'verify change at hiLabel', 'Howdy')
-//     await testRemote(t, 'wait 1000', 'wait 1 second to view changed values')
-//
-//     await testRemote(t, 'triggerAction nextbtn', 'press next button', true)
-//     await testRemote(t, 'wait 1000', 'wait 1 second to view changed page')
-//
-//     navInfo = await callRemote('readModelValue page.navInfo')
-//     t.ok(typeof navInfo === 'object', 'navInfo is an object (got '+typeof navInfo+') '+navInfo)
-//     t.ok(navInfo.pageId === 'next', 'Page is next (got '+navInfo.pageId+')')
-//     t.ok(navInfo.timestamp > firstTimestamp && navInfo.timestamp < Date.now(), 'Timestamp is valid (got '+navInfo.timestamp+')')
+    let navInfo = await callRemote('readModelValue page.navInfo')
+    t.ok(typeof navInfo === 'object', 'navInfo is an object (got '+typeof navInfo+') '+navInfo)
+    t.ok(navInfo.pageId === 'main', 'Page is main (got '+navInfo.pageId+')')
+    t.ok(navInfo.timestamp > 0 && navInfo.timestamp < Date.now(), 'Timestamp is valid (got '+navInfo.timestamp+')')
+    let firstTimestamp = navInfo.timestamp
+
+    await testRemote(t, 'assignComponent hiLabel simple-label', 'assign hiLabel', true)
+    await testRemote(t, 'assignComponent changer simple-button action changeValues', 'assign changer', true)
+    await testRemote(t, 'assignComponent nextbtn simple-button action nextPage', 'assign nextbtn', true)
+    await testRemote(t, 'readComponentProperty changer text', 'verify component', 'Change Values')
+    await testRemote(t, 'triggerAction changer', 'press button', true)
+
+    await testRemote(t, 'readModelValue greeting.hello', 'verify change at model', 'Howdy')
+//    await testRemote(t, 'readComponentProperty hiLabel text', 'verify change at hiLabel', 'Howdy')
+    await testRemote(t, 'wait 1000', 'wait 1 second to view changed values')
+
+    await testRemote(t, 'triggerAction nextbtn', 'press next button', true)
+    await testRemote(t, 'wait 1000', 'wait 1 second to view changed page')
+
+    navInfo = await callRemote('readModelValue page.navInfo')
+    t.ok(typeof navInfo === 'object', 'navInfo is an object (got '+typeof navInfo+') '+navInfo)
+    t.ok(navInfo.pageId === 'next', 'Page is next (got '+navInfo.pageId+')')
+    t.ok(navInfo.timestamp > firstTimestamp && navInfo.timestamp < Date.now(), 'Timestamp is valid (got '+navInfo.timestamp+')')
 
     await endTest(t)
 }
