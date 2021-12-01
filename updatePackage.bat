359c1455142a037bad7a20f11221ea6a26c3a88b
tremho <steve@ohmert.com>
2021-12-01 09:28:08 -0800

    Added updatePackage.bat

---
+++ updatePackage.bat
@@ -0,0 +1,10 @@
+
+@echo off
+@echo "updating jove modules"
+call npm uninstall @tremho/jove-desktop @tremho/jove-common @tremho/jove-cli
+call npm install @tremho/jove-desktop @tremho/jove-common
+call npm install --save-dev @tremho/jove-cli
+@echo "final install"
+call npm install
+@echo "building..."
+call npm run build --clean
