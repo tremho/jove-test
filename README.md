11be74e6f575c10fa1f6406308931e2554af51d7
Tremho <steve@ohmert.com>
2021-12-20 12:20:09 -0800

    named tree

--- README.md
+++ README.md
@@ -1033,3 +1033,5 @@ named tree
 named tree
 ..
 named tree
+..
+named tree
--- tests/remote/repeatCheck.ts
+++ tests/remote/repeatCheck.ts
@@ -12,11 +12,11 @@ export async function repeatCheck(t: any) {
 
     await callRemote('goToPage next-page')
 
-    // const tv = await callRemote('tree')
-    // console.log('tree-view', tv)
+    const tv = await callRemote('tree')
+    console.log('tree-view', tv)
 
     await testRemote(t, 'assignComponent rptfor repeat-for-each', 'identify rptfor', true)
-    const rfv = await callRemote('tree')
+    const rfv = await callRemote('tree rptfor')
     console.log('--------------------------------------------')
     console.log('repeat-for tree', rfv)
     console.log('--------------------------------------------')
